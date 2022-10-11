b65756adee4da6d6e90d8d7b7470d6be56794570
Volodymyr Anokhin <vlnn-github@proton.me>
2022-10-11 12:02:12 +0300

    Remove anki integration. Better solution needed.

--- config.el
+++ config.el
@@ -141,42 +141,6 @@
   )
 ;;}}}
 
-(use-package! anki-editor
-  :commands (anki-editor-mode)
-  :init
-  (map! :leader
-        :desc "Anki Push tree"
-        "m a p" #'anki-editor-push-tree)
-  :hook (org-capture-after-finalize . anki-editor-reset-cloze-number) ; Reset cloze-number after each capture.
-  :config
-  (setq anki-editor-create-decks t ;; Allow anki-editor to create a new deck if it doesn't exist
-        anki-editor-org-tags-as-anki-tags t
-        anki-editor-break-consecutive-braces-in-latex t)
-
-  (defun anki-editor-cloze-region-auto-incr (&optional arg)
-    "Cloze region without hint and increase card number."
-    (interactive)
-    (anki-editor-cloze-region my-anki-editor-cloze-number "")
-    (setq my-anki-editor-cloze-number (1+ my-anki-editor-cloze-number))
-    (forward-sexp))
-  (defun anki-editor-cloze-region-dont-incr (&optional arg)
-    "Cloze region without hint using the previous card number."
-    (interactive)
-    (anki-editor-cloze-region (1- my-anki-editor-cloze-number) "")
-    (forward-sexp))
-  (defun anki-editor-reset-cloze-number (&optional arg)
-    "Reset cloze number to ARG or 1"
-    (interactive)
-    (setq my-anki-editor-cloze-number (or arg 1)))
-  (defun anki-editor-push-tree ()
-    "Push all notes under a tree."
-    (interactive)
-    (anki-editor-push-notes '(4))
-    (anki-editor-reset-cloze-number))
-  ;; Initialize
-  (anki-editor-reset-cloze-number)
-  )
-
 ;; {{{ Org-mode stuff
 (after! org
   (add-to-list 'org-modules 'org-habit)
