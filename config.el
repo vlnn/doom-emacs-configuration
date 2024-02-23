6cf82bff5228533746152f5ee73bf9224ab0acde
Volodymyr Anokhin <vlnn-github@proton.me>
2024-02-23 10:43:39 +0200

    Reassign parinfer's hot keys
    Parinfer blocks the CIDER's rightful hot keys for printing results

--- config.el
+++ config.el
@@ -108,16 +108,28 @@
   (add-to-list 'exec-path "/home/va/.asdf/shims")
   (setq nrepl-use-ssh-fallback-for-remote-hosts t) ;; Cider should be able to connect to remote hosts using ssh
   (setq cider-eldoc-display-context-dependent-info t)
-  (setq clojure-align-forms-automatically t)
+  ;(setq clojure-align-forms-automatically t)
   (map! :after clojure-mode :map clojure-mode-map :localleader ;; faster simpler workflow shortcuts
         ("f" #'consult-flycheck)
         ("m" #'cider-selector)
         ("P" nil)
+        ("p" nil)
         ("\;" #'cider-pprint-eval-last-sexp-to-comment)
         ("N" #'cider-test-run-ns-tests)
         ("T" #'projectile-toggle-between-implementation-and-test))
+  (setq cider-print-options '(("length" 20) ("right-margin" 70)))
   (evil-make-intercept-map cider--debug-mode-map 'normal) ;; don't mess evil-mode with cider debug
+  (add-hook 'cider-repl-mode-hook '(lambda () (setq scroll-conservatively 101)))
   (add-hook 'cider-inspector-mode-hook #'evil-normalize-keymaps))
+
+(use-package! parinfer-rust-mode
+  :config
+  (map! :map parinfer-rust-mode-map
+        :localleader
+        "p" #'cider-pprint-eval-last-sexp
+        "[" #'parinfer-rust-switch-mode
+        "P" #'cider-pprint-eval-last-sexp-to-comment
+        "{" #'parinfer-rust-toggle-disable))
 
 ;; To kill trailing whitespaces... but being sane killer not removing whitespace I just typed in
 (use-package! ws-butler
